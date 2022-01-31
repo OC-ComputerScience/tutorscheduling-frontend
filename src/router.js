e489a4579967cc79668bd537b9cc7cc69e97dbc5
Grace Hopper <ghopp01@gmail.com>
2022-01-31 10:38:11 -0600

    start program structure for groups
    In this commit I started router.js, the service.js files, and group views.
    
    I started a file structure for the Views file, which will help with organization, especially since we have so many pages this project. Remember to check your path when writing connections in router.js.

---
+++ src/router.js
@@ -0,0 +1,67 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Login from './views/Login.vue'
+
+// Admin Views. Views are sorted into sections for this project, so check that the path is correct below.
+import GroupList    from './views/AdminViews/GroupViews/GroupList.vue';
+import GroupEdit    from './views/AdminViews/GroupViews/GroupEdit.vue';
+import GroupAdd     from './views/AdminViews/GroupViews/GroupAdd.vue';
+import GroupView    from './views/AdminViews/GroupViews/GroupView.vue';
+
+Vue.use(Router)
+
+const router =  new Router({
+  mode: 'history',
+  linkExactActiveClass: 'active',
+  base:
+    process.env.NODE_ENV === 'development'? "/" : "/tutoring/",
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/groupList',
+      name: 'groupList',
+      component: GroupList
+    },
+    {
+      path: '/groupEdit/:id',
+      name: 'groupEdit',
+      component: GroupEdit,
+      props: true
+    },
+    {
+      path: '/groupAdd',
+      name: 'groupAdd',
+      component: GroupAdd
+    },
+    {
+      path: '/groupView/:id',
+      name: 'groupView',
+      component: GroupView,
+      props: true
+    },
+    /*
+    {
+      path: '*',
+      component: NotFoundComponent
+    }
+    */
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login']
+  const authRequired = !publicPages.includes(to.path)
+  const loggedIn = localStorage.getItem('user')
+  if (authRequired && !loggedIn) {
+    return next('/login')
+  }
+  next()
+})
+
+export default router;
