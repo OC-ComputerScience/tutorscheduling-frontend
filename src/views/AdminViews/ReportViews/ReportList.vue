62d765909b6e31be68dad1eec28baa8d598ecf53
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-04-18 18:30:49 -0500

    more work on reporting

--- src/views/AdminViews/ReportViews/ReportList.vue
+++ src/views/AdminViews/ReportViews/ReportList.vue
@@ -8,12 +8,48 @@
         <br><br>
         <h4>Filter the table below and then download the csv file.</h4>
         <br><br>
+        <v-col md="4">
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="dateRangeText"
+                label="Date Range"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="dates"
+              range
+            ></v-date-picker>
+          </v-menu>
+        </v-col>
+        <v-col>
+          <v-select
+            :items="topics"
+            item-text="name"
+            item-value="id"
+            label="Topic"
+            @change="this.selectedTopic = topic"
+          >
+          </v-select>
+        </v-col>
+        <br><br>
         <v-card>
             <v-card-title>
-                Appointments for {{this.user.selectedGroup}} Report
+                Appointments for {{this.user.selectedGroup}} Reporting
                 <v-spacer></v-spacer>
                 <vue-json-to-csv
-                    :json-data="appointments"
+                    :json-data="selectedAppointments"
                     :labels="labels"
                     :csv-title="fileName"
                 >
@@ -48,37 +84,53 @@ import GroupServices from "@/services/groupServices.js";
     },
     data() {
       return {
-        search: '',
+        menu: false,
+        dates: [],
         group: {},
         user: {},
+        topics: [],
+        selectedTopic: '',
         fileName: '',
         today: '',
         appointments: [],
+        selectedAppointments: [],
         labels: { date: { title: 'Date' },
                   startTime: { title: 'Start' },
                   endTime: { title: 'End' },
                   type: { title: 'Type' },
                   status: { title: 'Status' },
-                  `topic.name`: { title: 'Pre Session Notes' },
+                  topicName: { title: 'Topic' },
+                  locationName: { title: 'Location' },
+                  locationBuilding: { title: 'Building' },
                   preSessionInfo: { title: 'Pre Session Notes' },
                   tutorFeedback: { title: 'Tutor Feedback' },
-                  studentFeedback: { title: 'Student Feedback' }},
+                  studentFeedback: { title: 'Student Feedback' },
+                  tutorStart: { title: 'Tutor Start' },
+                  tutorEnd: { title: 'Tutor End' }},
         headers: [{text: 'Date', value: 'date'}, 
                   {text: 'Start Time', value: 'startTime'},
                   {text: 'End Time', value: 'endTime'},
                   {text: 'Topic', value: 'topic.name'}]
       }
     },
+    computed: {
+      dateRangeText () {
+        return this.dates.join(' ~ ')
+      },
+    },
     async created() {
       this.user = Utils.getStore('user');
       await this.getGroup(this.user.selectedGroup.replace(/%20/g, " "))
-      .then(() => {
-        this.getAppointmentsForGroup();
-        var date = new Date();
-        this.today = String(date.getMonth() + 1).padStart(2, '0') + '-'
-            + String(date.getDate()).padStart(2, '0') + '-' 
-            + date.getFullYear();
-        this.fileName = this.user.selectedGroup + " Report for " + this.today;
+      .then(async () => {
+        await this.getAppointmentsForGroup()
+        .then(() => {
+          var date = new Date();
+          this.today = String(date.getMonth() + 1).padStart(2, '0') + '-'
+              + String(date.getDate()).padStart(2, '0') + '-' 
+              + date.getFullYear();
+          this.fileName = this.user.selectedGroup + " Report for " + this.today;
+          this.setSelectedAppointments();
+        })
       })
     },
     methods: {
@@ -90,6 +142,20 @@ import GroupServices from "@/services/groupServices.js";
         .catch((error) => {
           console.log("There was an error:", error.response);
         });
+      },
+      setSelectedAppointments() {
+        // filter appointments by date
+        if(this.dates.length > 0) {
+          this.selectedAppointments = this.appointments.filter(appointment => appointment.date > this.dates[0] && appointment.date < this.dates[1]);
+        }
+        else  // if no dates are picked, it will add all appointments
+          this.selectedAppointments = this.appointments;
+        for(let i = 0; i < this.appointments.length; i++) {
+          this.selectedAppointments[i].topicName = this.appointments[i].topic.name;
+          this.selectedAppointments[i].locationName = this.appointments[i].location.name;
+          this.selectedAppointments[i].locationBuilding = this.appointments[i].location.building;
+        }
+        console.log(this.selectedAppointments);
       },
       async getAppointmentsForGroup() {
         await AppointmentServices.getAppointmentForGroup(this.group.id)
