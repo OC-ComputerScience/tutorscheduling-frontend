7d74968636b254ef19918144e3db2399c9f0c1ee
Joshua-Stewart1 <jasbrickmaster@gmail.com>
2022-04-20 12:22:01 -0500

    fix: fixing overwrite issues

--- src/views/AdminViews/AvailabilityViews/AvailabilityAdd.vue
+++ src/views/AdminViews/AvailabilityViews/AvailabilityAdd.vue
@@ -94,6 +94,7 @@
           v-if="menu2"
           v-model="startTime"
           full-width
+          :allowed-minutes="this.allowedStep"
           @click:minute="$refs.menu.save(startTime)"
         ></v-time-picker>
 
@@ -129,6 +130,7 @@
           v-model="endTime"
           full-width
           @click:minute="$refs.menu.save(endTime)"
+          :allowed-minutes="this.allowedStep"
         ></v-time-picker>
 
         
@@ -268,7 +270,7 @@ import PersonAppointmentServices from "@/services/personAppointmentServices.js"
           this.appointment.date = date
           this.appointment.startTime = this.startTime
           this.appointment.endTime = this.endTime
-          this.appointment.type = "Single Session"
+          this.appointment.type = this.person.fName + " " + this.person.lName
           this.appointment.status = "available"
           this.appointment.groupId = this.group.id
           this.appointment.locationId = this.location.id
@@ -356,6 +358,7 @@ import PersonAppointmentServices from "@/services/personAppointmentServices.js"
       GroupServices.getGroupByName(name)
       .then((response) => {
         this.group = response.data[0]
+        console.log(this.group)
         this.getTopicsForGroup()
       })
       .catch((error) => {
@@ -366,14 +369,18 @@ import PersonAppointmentServices from "@/services/personAppointmentServices.js"
       TopicServices.getTopicForPerson(this.person.id)
       .then(response => {
         this.topic = response.data[0]
+        console.log(this.topic)
         LocationServices.getAllForGroup(this.group.id).then(response => {
           this.location = response.data[0]
+          console.log(this.location)
         })
       })
       .catch(error => {
         console.log("There was an error:", error.response)
       });
     },
+
+    allowedStep: m => m % 30 === 0,
 
     // popup functions
     deleteItem (item) {
@@ -386,7 +393,7 @@ import PersonAppointmentServices from "@/services/personAppointmentServices.js"
         AvailabilityServices.deleteAvailability(this.editedItem.id)
           .then(() => {
             this.close()
-            window.location.reload();          
+            this.getAvailabilities()      
           })
           .catch(error => {
             console.log("There was an error:", error.response)
