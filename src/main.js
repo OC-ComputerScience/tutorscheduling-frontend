9e23691e8c66eaf725454cacf08c9d78bc0d3846
David North <david.north@oc.edu>
2022-06-17 07:26:29 -0500

    fix path for app for windows

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import vuePdf from 'vue-pdf'
 import GoogleAuth from '@/config/google.js'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+require('dotenv').config()
+
 const gauthOption = {
   clientId: '158532899975-5qk486rajjjb3dqrdbp4h86a65l997ab.apps.googleusercontent.com',
   scope: 'profile email',
