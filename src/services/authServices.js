8ed49bcde6ea5b218913be90e04e3c90dae67e92
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-07-09 15:36:41 -0500

    start on google cal

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -3,15 +3,6 @@ import apiClient from "@/services/services.js";
 export default {
   loginUser(user) {
     return apiClient.post("login", user);
-    // , {
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json",
-    //     "X-Requested-With": "XMLHttpRequest",
-    //     crossDomain: true,
-    //     Authorization: "Basic " + btoa(user.username + ":" + user.password)
-    //   }
-    // });
   },
   logoutUser(token) {
     return apiClient.post("logout", token);
--- src/views/TutorViews/TutorHome.vue
+++ src/views/TutorViews/TutorHome.vue
@@ -117,6 +117,7 @@ import GroupServices from "@/services/groupServices.js";
       };
     },
     async created() {
+      this.testAuthorization();
       this.user = Utils.getStore('user');
       if(this.id !== 0) {
         this.getTutorRole();
@@ -128,6 +129,30 @@ import GroupServices from "@/services/groupServices.js";
       })
     },
     methods: {
+      testAuthorization() {
+        const client = global.google.accounts.oauth2.initCodeClient({
+          client_id: process.env.VUE_APP_CLIENT_ID,
+          scope: 'https://www.googleapis.com/auth/calendar',
+          ux_mode: 'popup',
+          callback: (response) => {
+            console.log(response);
+            var code_receiver_uri = 'authorize';
+            // Send auth code to your backend platform
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', code_receiver_uri, true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhr.onload = function() {
+              console.log('Signed in as: ' + xhr.responseText);
+            };
+            xhr.send('code=' + response.code);
+            // After receipt, the code is exchanged for an access token and
+            // refresh token, and the platform then updates this web app
+            // running in user's browser with the requested calendar info.
+          },
+        });
+        client.requestCode();
+      },
       async getGroup(name) {
         await GroupServices.getGroupByName(name)
         .then((response) => {
