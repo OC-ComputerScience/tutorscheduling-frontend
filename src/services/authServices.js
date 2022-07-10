b5dc8c412a3b6b6f8181637d937ed30615794555
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-07-10 15:55:34 -0500

    tokens generate now

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -4,6 +4,9 @@ export default {
   loginUser(user) {
     return apiClient.post("login", user);
   },
+  authorizeUser(code) {
+    return apiClient.post("authorize", code);
+  },
   logoutUser(token) {
     return apiClient.post("logout", token);
   }
