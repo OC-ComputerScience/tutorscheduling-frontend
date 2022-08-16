90a1e3fb44dc1e7aa3c5e4af5243aefcff22ccd6
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-08-16 09:56:49 -0500

    fully added pdfs to help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -2,49 +2,16 @@
     <div>
         <v-container>
             <v-toolbar>
-                <v-toolbar-title>How to use this service:</v-toolbar-title>
+                <v-toolbar-title>How to use this application:</v-toolbar-title>
             </v-toolbar>
             <br><br>
-            <button @click="$refs.myPdfComponent.print()">print</button>
+            <!-- <button @click="$refs.myPdfComponent.print()">print</button> -->
             <pdf
                 v-for="i in tutorial.pages"
                 :key="i"
                 :page="i"
                 :src="tutorial.pdf"
             ></pdf>
-            <v-row justify="center">
-                <v-col justify="center">
-                    <v-card 
-                        color="error"
-                    >
-                        <v-card-title class="justify-center white--text">Student</v-card-title>
-                        <v-card-text class="white--text">
-                            <h3>Appointments:</h3>
-                            <p>Make an account using your OC login information.</p>
-                            <p>Select which organizations you would like to receive tutoring from.</p>
-                            <p>Sign a contract for each organization.</p>
-                            <p>Find a time that works for the subject you need help for in the calendar.</p>
-                            <p>Sign up for that appointment time.</p>
-                            <h3>Requests:</h3>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-                <v-col justify="center">
-                    <v-card 
-                        color="darkblue"
-                    >
-                        <v-card-title class="justify-center white--text">Tutor</v-card-title>
-                        <v-card-text class="white--text">
-                            <p>Make an account using your OC login information.</p>
-                            <p>Select which organizations you would like to tutor for.</p>
-                            <p>Sign a contract for each organization.</p>
-                            <p>Wait for the organization supervisors to approve your application.</p>
-                            <p>Set up the times you are available to tutor.</p>
-                            <p>Accept appointments that are set up by students.</p>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-            </v-row>
         </v-container>
     </div>
 </template>
