71fc35179da73155cee163d57af6a63aa90bed64
Chloe Sheasby <chloesheasby@gmail.com>
2023-03-08 16:33:31 -0600

    removed the general text route

--- src/services/twilioServices.js
+++ src/services/twilioServices.js
@@ -1,9 +1,6 @@
 import apiClient from "@/services/services.js";
 
 export default {
-  sendMessage(user) {
-    return apiClient.post("twilio/sendMessage", user);
-  },
   sendApplicationMessage(textInfo) {
     return apiClient.post(`twilio/sendApplication`, textInfo);
   },
