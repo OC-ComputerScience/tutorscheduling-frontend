3e2ad49d4d581756a9c97295eac59b2fb1fdd4d6
Chloe Sheasby <chloesheasby@gmail.com>
2023-03-16 16:01:03 -0500

    consolidated a redirect function

--- src/services/services.js
+++ src/services/services.js
@@ -21,33 +21,37 @@ const apiClient = axios.create({
   },
 });
 
+const redirectToLogin = async () => {
+  if (Router.currentRoute.path !== "/") {
+    await Router.push({
+      path: "/",
+      query: { redirect: Router.currentRoute.fullPath },
+    }).catch((err) => {
+      // Ignore the vuex err regarding navigating to the page they are already on.
+      if (
+        err.name !== "NavigationDuplicated" &&
+        !err.message.includes(
+          "Avoided redundant navigation to current location"
+        )
+      ) {
+        // But print any other errors to the console
+        console.log(err);
+      }
+    });
+  }
+};
+
 const authInterceptor = async (config) => {
   let user = Utils.getStore("user");
   if (user !== null && user !== undefined) {
     if (user.sessionExpirationDate < new Date()) {
-      console.log(Router.currentRoute);
-      if (Router.currentRoute.path !== "/") {
-        await Router.push({
-          path: "/",
-          query: { redirect: Router.currentRoute.fullPath },
-        }).catch((err) => {
-          // Ignore the vuex err regarding navigating to the page they are already on.
-          if (
-            err.name !== "NavigationDuplicated" &&
-            !err.message.includes(
-              "Avoided redundant navigation to current location"
-            )
-          ) {
-            // But print any other errors to the console
-            console.log(err);
-          }
-        });
-      }
+      await redirectToLogin();
     }
     if (user.token != null && user.token != "") {
       config.headers.Authorization = `Bearer ${user.token}`;
     }
   }
+
   return config;
 };
 
@@ -58,20 +62,14 @@ apiClient.interceptors.response.use(
     return response;
   },
   async function (error) {
-    console.log(error);
-    // fix expired token issue
     if (
       Utils.getStore("user") !== null &&
       Utils.getStore("user") !== undefined &&
       error.response.status === 498
     ) {
-      await AuthServices.logoutUser(Utils.getStore("user"))
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      await AuthServices.logoutUser(Utils.getStore("user")).catch((error) => {
+        console.log(error);
+      });
     }
 
     if (
@@ -85,24 +83,8 @@ apiClient.interceptors.response.use(
       ) {
         Utils.removeItem("user");
       }
-      console.log(Router.currentRoute);
-      if (Router.currentRoute.path !== "/") {
-        await Router.push({
-          path: "/",
-          query: { redirect: Router.currentRoute.fullPath },
-        }).catch((err) => {
-          // Ignore the vuex err regarding navigating to the page they are already on.
-          if (
-            err.name !== "NavigationDuplicated" &&
-            !err.message.includes(
-              "Avoided redundant navigation to current location"
-            )
-          ) {
-            // But print any other errors to the console
-            console.log(err);
-          }
-        });
-      }
+
+      await redirectToLogin();
     }
 
     return Promise.reject(error);
